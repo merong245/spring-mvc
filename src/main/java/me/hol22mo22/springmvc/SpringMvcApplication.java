ac0edd8db5820d8e232267955ee5a2d088877987
이준혁 <merong245@gmail.com>
2022-08-01 17:01:08 +0900

    Spring MVC init

---
+++ src/main/java/me/hol22mo22/springmvc/SpringMvcApplication.java
@@ -0,0 +1,13 @@
+package me.hol22mo22.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hol22mo22/springmvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hol22mo22.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
