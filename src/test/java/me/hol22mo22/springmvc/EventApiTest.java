6bd68c0c00fe8e6afd2b886b07918be282ee7839
이준혁 <merong245@gmail.com>
2022-08-10 17:29:13 +0900

    핸들러 메소드 ResponseBody,HttpEntity

---
+++ src/test/java/me/hol22mo22/springmvc/EventApiTest.java
@@ -0,0 +1,65 @@
+package me.hol22mo22.springmvc;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class EventApiTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void createEvent() throws Exception {
+        Event event = new Event();
+
+        event.setName("jun");
+        event.setLimit(20);
+
+        String json = objectMapper.writeValueAsString(event);
+        mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").value(event.getName()))
+                .andExpect(jsonPath("limit").value(event.getLimit()))
+                ;
+
+    }
+
+    @Test
+    public void createHttpEvent() throws Exception {
+        Event event = new Event();
+
+        event.setName("jun");
+        event.setLimit(20);
+
+        String json = objectMapper.writeValueAsString(event);
+        mockMvc.perform(post("/api/events/http")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").value(event.getName()))
+                .andExpect(jsonPath("limit").value(event.getLimit()))
+        ;
+
+    }
+}
