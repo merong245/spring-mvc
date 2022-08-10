85b38ffea65a7937461c9a30cedf29f5d7d93f24
이준혁 <merong245@gmail.com>
2022-08-10 17:00:29 +0900

    핸들러 메소드 다운로드

--- src/main/java/me/hol22mo22/springmvc/FileController.java
+++ src/main/java/me/hol22mo22/springmvc/FileController.java
@@ -1,16 +1,26 @@
 package me.hol22mo22.springmvc;
 
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 public class FileController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     @GetMapping("/file")
     public String fileUploadForm(Model model){
         return "/files/index";
@@ -25,6 +35,20 @@ public class FileController {
         attributes.addFlashAttribute("message",message);
         return "redirect:/file";
 
+
+    }
+
+    @GetMapping("/file/{filename}")
+    public ResponseEntity<Resource> fileDownload(@PathVariable String filename) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + filename);
+        File file = resource.getFile();
+        Tika tika = new Tika();
+        String mediaType = tika.detect(file);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename())
+                .header(HttpHeaders.CONTENT_TYPE,mediaType)
+                .header(HttpHeaders.CONTENT_LENGTH,file.length() + "")
+                .body(resource);
 
     }
 }
--- src/test/java/me/hol22mo22/springmvc/FileControllerTest.java
+++ src/test/java/me/hol22mo22/springmvc/FileControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,7 +21,7 @@ class FileControllerTest {
     @Test
     public void fileUploadTest() throws Exception{
         MockMultipartFile file = new MockMultipartFile("file",
-                "test.txt",
+                "templates/test.txt",
                 "text/plain",
                 "hello file".getBytes());
         mockMvc.perform(multipart("/file").file(file))
